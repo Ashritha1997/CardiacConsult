b589a261422cb8e154c328b95eef5adf2d64dcdc
purvik.rana@thesunflowerlab.com <purvik.rana@thesunflowerlab.com>
2019-02-10 02:12:17 +0530

    changes done 10_02

--- app/src/main/java/com/project/cardiacconsult/activities/HomeActivity.java
+++ app/src/main/java/com/project/cardiacconsult/activities/HomeActivity.java
@@ -2,6 +2,7 @@ package com.project.cardiacconsult.activities;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,6 +10,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,38 +18,57 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.project.cardiacconsult.R;
 import com.project.cardiacconsult.adapters.MainMenuAdapter;
+import com.project.cardiacconsult.models.Clinic;
+import com.project.cardiacconsult.models.User;
+import com.project.cardiacconsult.utils.RoleEnum;
+import com.project.cardiacconsult.utils.SharedPrefHelper;
 
 
 public class HomeActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
     ProgressBar progressBar;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference clinicDatabaseReference;
+    SharedPrefHelper sharedPrefHelper;
+
+    User currentUser;
+    Clinic clinicInfo;
 
     private NavigationView navigationView;
     private DrawerLayout drawer;
     private Toolbar tool;
+    MainMenuAdapter mainMenuAdapter;
 
     GridView gridView;
 
-    String[] textview = {"ecg", "physician", "heartbeat"};
-    int[] Imageid = {R.drawable.logo, R.drawable.logo, R.drawable.logo};
+    TextView tvUserEmail, tvUserContact, tvUserAddress;
+    TextView tvClinicName, tvClinicAddress, tvClinicContact, tvClinicLicNo, tvClinicInfoTitle;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        setTitle("Home");
 
+        progressBar = findViewById(R.id.progressbar);
 
         tool = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(tool);
+        tool.setTitle("HOME");
 
         drawer=(DrawerLayout)findViewById(R.id.drawer_layout);
         navigationView=(NavigationView)findViewById(R.id.nav_view);
@@ -60,35 +81,79 @@ public class HomeActivity extends AppCompatActivity {
 
         drawer.openDrawer(GravityCompat.START);
 
-        setTitle("Home");
-
-        findViewById();
-
         auth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        sharedPrefHelper = new SharedPrefHelper(HomeActivity.this);
+
+        if (auth.getCurrentUser() != null) {
+            displayUserInfoInNavBar();
+        }
 
         gridView = findViewById(R.id.grid_view);
-        MainMenuAdapter adapter = new MainMenuAdapter(HomeActivity.this, textview, Imageid);
+        mainMenuAdapter = new MainMenuAdapter(HomeActivity.this);
 
-        gridView.setAdapter(adapter);
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        gridView.setAdapter(mainMenuAdapter);
+    }
 
+    private void displayUserInfoInNavBar() {
+
+        findViewByIdNavBar();
+
+        currentUser = sharedPrefHelper.getUserFromPref();
+        tvUserEmail.setText(currentUser.getEmail());
+        tvUserAddress.setText(currentUser.getAddress());
+        tvUserContact.setText(currentUser.getPhoneNO());
+
+        if (currentUser.getRole() == RoleEnum.PHYSICIAN) {
+            Toast.makeText(getApplicationContext(), "Is Physician", Toast.LENGTH_LONG).show();
+            getAndDisplayClinicInfoInNavBar(sharedPrefHelper.getUidFromPref());
+
+        } else {
+            Toast.makeText(getApplicationContext(),"Is Not Physician", Toast.LENGTH_LONG).show();
+            tvClinicInfoTitle.setVisibility(View.GONE);
+            tvClinicName.setVisibility(View.GONE);
+            tvClinicAddress.setVisibility(View.GONE);
+            tvClinicContact.setVisibility(View.GONE);
+            tvClinicLicNo.setVisibility(View.GONE);
+        }
+    }
+
+    private void getAndDisplayClinicInfoInNavBar(String uidFromPref) {
+
+        clinicDatabaseReference = firebaseDatabase.getReference("clinics");
+        clinicDatabaseReference.child(uidFromPref).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                Toast.makeText(HomeActivity.this, "You Clicked at " +textview[+ position], Toast.LENGTH_SHORT).show();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d("DataSnapshot", "onDataChange: " + dataSnapshot.toString());
 
+                clinicInfo = dataSnapshot.getValue(Clinic.class);
+                tvClinicName.setText(clinicInfo.getName());
+                tvClinicAddress.setText(clinicInfo.getAddress());
+                tvClinicContact.setText(clinicInfo.getContactNo());
+                tvClinicLicNo.setText(clinicInfo.getLicenceNo());
             }
 
-
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), "No Clinic Data Availabe", Toast.LENGTH_SHORT).show();
+            }
         });
-
-
-
 
     }
 
-    private void findViewById() {
-        progressBar = findViewById(R.id.progressbar);
+    private void findViewByIdNavBar() {
+
+        tvUserEmail = findViewById(R.id.tvEmail);
+        tvUserAddress = findViewById(R.id.tvAddress);
+        tvUserContact = findViewById(R.id.tvContact);
+
+        tvClinicInfoTitle = findViewById(R.id.tvTitleClinicData);
+        tvClinicName = findViewById(R.id.tvClinicName);
+        tvClinicAddress = findViewById(R.id.tvClinicAddress);
+        tvClinicContact = findViewById(R.id.tvClinicContact);
+        tvClinicLicNo = findViewById(R.id.tvClinicLicNo);
+
+
     }
 
     @Override
