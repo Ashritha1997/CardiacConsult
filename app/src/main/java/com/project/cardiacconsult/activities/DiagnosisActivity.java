ff6ed061097ef8ceeb9052e362bc4891f46398e3
unknown <mansi.150410116034@gmail.com>
2019-02-24 12:36:04 +0530

    Intent for home activity

---
+++ app/src/main/java/com/project/cardiacconsult/activities/DiagnosisActivity.java
@@ -0,0 +1,15 @@
+package com.project.cardiacconsult.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.cardiacconsult.R;
+
+public class DiagnosisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diagnosis);
+    }
+}
--- app/src/main/java/com/project/cardiacconsult/adapters/MainMenuAdapter.java
+++ app/src/main/java/com/project/cardiacconsult/adapters/MainMenuAdapter.java
@@ -1,20 +1,27 @@
 package com.project.cardiacconsult.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.project.cardiacconsult.R;
+import com.project.cardiacconsult.activities.DiagnosisActivity;
+import com.project.cardiacconsult.activities.ECGActivity;
+import com.project.cardiacconsult.activities.FeedbackActivity;
+import com.project.cardiacconsult.activities.HistoryActivity;
+import com.project.cardiacconsult.activities.PhysicianSearchActivity;
 
 public class MainMenuAdapter extends BaseAdapter {
 
     private Context mContext;
-    private final String[] menuLabels = {"ECG", "DETAILS", "HISTORY","ECG", "DETAILS", "HISTORY","ECG", "DETAILS", "HISTORY"};
-    private final int[] menuIds = {R.drawable.ic_hospital, R.drawable.ic_body_points, R.drawable.ic_ecg_history,R.drawable.ic_hospital, R.drawable.ic_body_points, R.drawable.ic_ecg_history,R.drawable.ic_hospital, R.drawable.ic_body_points, R.drawable.ic_ecg_history};
+    private final String[] menuLabels = {"ECG", "DIAGNOSIS", "HISTORY","PHYSICIAN SEARCH", "FEEDBACK"};
+    private final int[] menuIds = {R.drawable.ic_cardiogram, R.drawable.ic_body_points, R.drawable.ic_clinic_history,R.drawable.ic_doctor_search, R.drawable.ic_feedback_icon};
 
     //Constructor
     public MainMenuAdapter(Context c){
@@ -52,7 +59,48 @@ public class MainMenuAdapter extends BaseAdapter {
         } else {
             itemView = convertView;
         }
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView tv= view.findViewById(R.id.itemLabel);
+               // Toast.makeText(mContext, "Clicked: " + tv.getText(),Toast.LENGTH_LONG).show();
+                switch (tv.getText().toString())
+                {
+                    case "ECG":
+                        Intent i1=  new Intent(mContext.getApplicationContext(),ECGActivity.class);
+                        mContext.startActivity(i1);
+                        break;
+
+                    case "DIAGNOSIS":
+                        Intent i2=  new Intent(mContext.getApplicationContext(),DiagnosisActivity.class);
+                        mContext.startActivity(i2);
+                        break;
+
+                    case "HISTORY":
+                        Intent i3=  new Intent(mContext.getApplicationContext(),HistoryActivity.class);
+                        mContext.startActivity(i3);
+                        break;
+
+                    case "PHYSICIAN SEARCH":
+                        Intent i4=  new Intent(mContext.getApplicationContext(),PhysicianSearchActivity.class);
+                        mContext.startActivity(i4);
+                        break;
+
+                    case "FEEDBACK":
+                        Intent i5=  new Intent(mContext.getApplicationContext(),FeedbackActivity.class);
+                        mContext.startActivity(i5);
+                        break;
+
+                }
+                //Intent intent
+            }
+        });
+
+
         return itemView;
+
+
     }
+
 
 }
