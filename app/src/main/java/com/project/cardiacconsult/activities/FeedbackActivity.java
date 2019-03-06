89e20ca619c0095594f2e15c303db7d717b3ae81
unknown <ashrithabommagani@gmail.com>
2019-03-06 13:28:28 +0530

    Feedback Activity

--- app/src/main/java/com/project/cardiacconsult/activities/FeedbackActivity.java
+++ app/src/main/java/com/project/cardiacconsult/activities/FeedbackActivity.java
@@ -2,14 +2,67 @@ package com.project.cardiacconsult.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.project.cardiacconsult.R;
+import com.project.cardiacconsult.models.Feedback;
 
-public class FeedbackActivity extends AppCompatActivity {
+public class FeedbackActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Toolbar toolbar;
+    Feedback feedback;
+    RatingBar ratingBar1, ratingBar2, ratingBar3, ratingBar4;
+    Button btfeedback;
+    FirebaseDatabase database;
+    FirebaseAuth auth;
+    FirebaseUser firebaseUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Feedback");
+
+
+        ratingBar1=findViewById(R.id.ratingBar1);
+        ratingBar2=findViewById(R.id.ratingBar2);
+        ratingBar3=findViewById(R.id.ratingBar3);
+        ratingBar4=findViewById(R.id.ratingBar4);
+
+        btfeedback=findViewById(R.id.btfeedback);
+        btfeedback.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        float overallrating=ratingBar1.getRating();
+        float accuracy=ratingBar2.getRating();
+        float diagnosis=ratingBar3.getRating();
+        float phyrating=ratingBar4.getRating();
+
+        feedback=new Feedback(overallrating, accuracy, diagnosis, phyrating);
+        storefeedbackdetails(feedback);
+
+    }
+
+    public void storefeedbackdetails(Feedback feedback)
+    {
+
+        firebaseUser = auth.getCurrentUser();
+        DatabaseReference feedbackRef = database.getReference("feedback");
+        feedbackRef.child(firebaseUser.getUid()).setValue(feedback);
+
     }
 }
---
+++ app/src/main/java/com/project/cardiacconsult/models/Feedback.java
@@ -0,0 +1,60 @@
+package com.project.cardiacconsult.models;
+
+public class Feedback {
+
+
+    public float Overallrating;
+
+    public float accuracy;
+
+    public float diagnosis;
+
+    public float phyrating;
+
+    public Feedback(){
+
+    }
+
+    public Feedback(float overallrating, float accuracy, float diagnosis, float phyrating)
+    {
+        Overallrating=overallrating;
+        this.accuracy=accuracy;
+        this.diagnosis=diagnosis;
+        this.phyrating=phyrating;
+
+    }
+
+    public void setOverallrating(float overallrating) {
+        Overallrating = overallrating;
+    }
+
+    public void setAccuracy(float accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public void setDiagnosis(float diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public void setPhyrating(float phyrating) {
+        this.phyrating = phyrating;
+    }
+
+
+
+    public float getOverallrating() {
+        return Overallrating;
+    }
+
+    public float getAccuracy() {
+        return accuracy;
+    }
+
+    public float getDiagnosis() {
+        return diagnosis;
+    }
+
+    public float getPhyrating() {
+        return phyrating;
+    }
+}
